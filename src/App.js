caccb71f49f33ab9c55d1d1031c2086c34483309
Kate Hooker <katehooker88@gmail.com>
2023-10-16 02:19:19 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  const APP_ID = 'dcd610e5'
+  const APP_Key = '0ae5ba87c9bd4635c091a3a7963f6541'
+
+  return (
+    <div className="App">
+      <h1>Hello react</h1>
+    </div>
+  )
+}
+
+export default App
