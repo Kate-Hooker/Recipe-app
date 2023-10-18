18f0cb00f8aeb5d8c26df96f43ff593b264bab87
Kate Hooker <katehooker88@gmail.com>
2023-10-18 08:24:59 +0000

    api call not working, new one is working

--- src/App.js
+++ src/App.js
@@ -6,15 +6,23 @@ const App = () => {
   const APP_ID = 'dcd610e5'
   const APP_KEY = '0ae5ba87c9bd4635c091a3a7963f6541'
 
-  const exampleReq = `https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}`
+  //const exampleReq = `https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}`
 
-  const [counter, setCounter] = useState(0)
+  //const [counter, setCounter] = useState(0)
 
   useEffect(() => {
-    console.log('effect has been run')
+    //console.log('effect has been run')
+    getRecipes()
   }, [])
 
-  const getRecipes 
+  const getRecipes = async () => {
+    const response = await fetch(
+      //`https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_0123456789abcdef0123456789abcdef&app_id=${APP_ID}&app_key=${APP_KEY}`
+    )
+    const data = await response.json()
+    console.log(data)
+  }
 
   return (
     <div className="App">
@@ -24,7 +32,6 @@ const App = () => {
           Search
         </button>
       </form>
-
     </div>
   )
 }
