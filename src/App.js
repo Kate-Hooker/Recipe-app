4b6fd1866db6f25dcec2c9a3500ba145632fc768
Kate Hooker <katehooker88@gmail.com>
2023-10-18 00:03:18 +0000

    search bar added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import './App.css'
 
 const App = () => {
   const APP_ID = 'dcd610e5'
-  const APP_Key = '0ae5ba87c9bd4635c091a3a7963f6541'
+  const APP_KEY = '0ae5ba87c9bd4635c091a3a7963f6541'
+
+  const exampleReq = `https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}`
 
   return (
     <div className="App">
+      <form className="search-from">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
       <h1>Hello react</h1>
     </div>
   )
