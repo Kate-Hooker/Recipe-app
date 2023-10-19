664bdc04c06ea182345100c4eb2dc1dee2ae5ab8
Kate Hooker <katehooker88@gmail.com>
2023-10-19 13:33:21 +1300

    recipe call successful

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const APP_ID = 'dcd610e5'
-  const APP_KEY = '0ae5ba87c9bd4635c091a3a7963f6541'
+  const APP_ID = process.env.APP_ID //
+  const APP_KEY = process.env.APP_KEY //
 
   //const exampleReq = `https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}`
 
@@ -18,7 +18,7 @@ const App = () => {
   const getRecipes = async () => {
     const response = await fetch(
       //`https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=${APP_ID}&app_key=${APP_KEY}`
-      `https://api.edamam.com/search?r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_0123456789abcdef0123456789abcdef&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=tuna&_app_id=${APP_ID}&app_key=${APP_KEY}`
     )
     const data = await response.json()
     console.log(data)
